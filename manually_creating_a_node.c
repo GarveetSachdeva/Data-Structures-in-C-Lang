8924fbf80b95d4a24c01c8899be975d2d682ad2e
GarveetSachdeva <149353681+GarveetSachdeva@users.noreply.github.com>
2025-08-02 21:58:51 +0530

    Update manually_creating_a_node.c

--- manually_creating_a_node.c
+++ manually_creating_a_node.c
@@ -15,20 +15,19 @@ int main()
     nodes[0] = (struct node *)malloc(sizeof(struct node));
     nodes[0]->data = 100;
 
-    start=&(nodes[0]->data);
+    start = &(nodes[0]->data);
 
     nodes[1] = (struct node *)malloc(sizeof(struct node));
     nodes[1]->data = 200;
     nodes[1]->link = NULL;
 
-    nodes[0]->link = nodes[1];   // linking address of nodes[1] to nodes[0]
-    
-    printf("%d",nodes[0]->link->data);
+    nodes[0]->link = nodes[1]; // linking address of nodes[1] to nodes[0]
+
+    printf("%d", nodes[0]->link->data);
     printf("\n%d", *start);
 
     free(nodes[0]);
     free(nodes[1]);
-    
+
     return 0;
 }
-
